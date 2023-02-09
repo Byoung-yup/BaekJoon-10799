ccf8701c525540fc09a5bdde4678f6ade4316ecb
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-02-10 02:24:55 +0900

    commit

--- BaekJoon#10799/main.swift
+++ BaekJoon#10799/main.swift
@@ -4,8 +4,33 @@
 //
 //  Created by 김병엽 on 2023/02/10.
 //
+// Reference: https://velog.io/@sun02/Swift-백준-10799-쇠막대기
 
 import Foundation
 
-print("Hello, World!")
+func solution() -> Int {
 
+    let input = readLine()!.map { String($0) }
+
+    var stack = 0
+    var count = 0
+
+    for i in 0..<input.count {
+
+        if input[i] == "(" {
+            stack += 1
+        } else {
+            stack -= 1
+            if input[i - 1] == "(" {
+                count += stack
+            } else {
+                count += 1
+            }
+        }
+
+    }
+
+    return count
+}
+
+print(solution())
